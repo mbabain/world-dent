c068f6595e1f3e93e92d42756828fdab90ffbae9
Maksim Babain <mababain@mail.ru>
2020-09-02 14:02:37 +0700

    First commit

---
+++ js/main.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+    $("#first-block-slider").owlCarousel({
+        items: 1,
+        loop: true
+    });
+  });
+$(document).ready(function(){
+    $("#services-slider").owlCarousel({
+        stagePadding: 80,
+        items: 2.6 ,
+        margin: 12,
+        loop: true
+    });
+});
+$(document).ready(function(){
+    $("#discounts-slider").owlCarousel({
+        stagePadding: 200,
+        items: 2,
+        margin: 22,
+        loop: true
+    });
+});
